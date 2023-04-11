cc92640aeeb580a207dfbe18528ad92286276282
Minouccc <343723899@qq.com>
2023-04-11 16:11:13 +0800

    feat: add goodPriceInfo

--- src/store/modules/home.js
+++ src/store/modules/home.js
@@ -1,9 +1,26 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { getHomeGoodPriceData } from "@/services";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 
+export const fetchHomeDataAction = createAsyncThunk("fetchdata", async () => {
+  const res = await getHomeGoodPriceData();
+  return res;
+});
 const homeSlice = createSlice({
   name: "home",
-  initialState: {},
-  reducers: {},
+  initialState: {
+    goodPriceInfo: {},
+  },
+  reducers: {
+    changeGoodPrcieInfoAction(state, { payload }) {
+      state.goodPriceInfo = payload;
+    },
+  },
+  extraReducers: {
+    [fetchHomeDataAction.fulfilled](state, { payload }) {
+      state.goodPriceInfo = payload;
+    },
+  },
 });
 
+export const { changeGoodPrcieInfoAction } = homeSlice.actions;
 export default homeSlice.reducer;
--- src/views/home/index.jsx
+++ src/views/home/index.jsx
@@ -1,12 +1,27 @@
-import React, { memo } from 'react'
+import React, { memo, useEffect } from 'react'
+import { shallowEqual, useDispatch, useSelector } from 'react-redux'
+
 import { HomeWrapper } from './style'
 import HomeBannner from './c-cpns/home-banner'
+import { fetchHomeDataAction } from '@/store/modules/home'
+
 
 const Home = memo(() => {
-  
+  const {goodPriceInfo} = useSelector((state) => ({
+    goodPriceInfo: state.home.goodPriceInfo
+  }),shallowEqual)
+
+  const dispatch = useDispatch()
+  useEffect(() => {
+    dispatch(fetchHomeDataAction())
+  },[dispatch])
+
   return (
     <HomeWrapper>
       <HomeBannner />
+      <div className="content">
+          <h2>{goodPriceInfo.title}</h2>
+      </div>
     </HomeWrapper>
   )
 })
