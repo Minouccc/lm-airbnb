d99e836e7b8a9763cb904df24dc487657ad8e634
Minouccc <343723899@qq.com>
2023-04-18 16:10:50 +0800

    feat: add EntireRooms

--- src/views/entire/index.jsx
+++ src/views/entire/index.jsx
@@ -1,12 +1,20 @@
-import React, { memo } from 'react'
+import React, { memo, useEffect } from 'react'
 import { EntireWrapper } from './style'
 import EntireFilter from './c-cpns/entire-filter'
+import { useDispatch } from 'react-redux'
+import { fetchRoomListAction } from '@/store/modules/entire/actionCreators'
+import EntireRoom from './c-cpns/entire-rooms'
 
 const Entire = memo(() => {
+  const dispatch = useDispatch()
+  useEffect(()=>{
+    dispatch(fetchRoomListAction())
+  },[dispatch])
+
   return (
     <EntireWrapper>
       <EntireFilter />
-      <div className="rooms">room-section</div>
+      <EntireRoom />
       <div className="pagination">pagination-section</div>
     </EntireWrapper>
   )
